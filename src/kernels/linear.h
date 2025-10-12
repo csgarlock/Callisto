5a7b40f41607dea6d18b7cc04ff93927bdd72a8c
Csgarlock <csgarlock@gmail.com>
2025-10-12 02:43:33 -0400

    too tired to figure the rest out

--- src/kernels/linear.h
+++ src/kernels/linear.h
@@ -23,9 +23,13 @@ __device__ __forceinline__ void fill_weights(const float *__restrict__ src, floa
 
 template <bool Batch>
 __device__ __forceinline__ void fill_input(const float *__restrict__ src, float *__restrict__ des, int x_offset, int k_offset, int n) {
-    const float4 *src4 = reinterpret_cast<const float4*>(src);
-    float4 *des4 = reinterpret_cast<float4*>(des);
-    if constexpr (!Batch) {
+    if constexpr (Batch) {
+        const float4 *src4 = reinterpret_cast<const float4*>(src);
+        float4 *des4 = reinterpret_cast<float4*>(des);
+        int effective_k = (threadIdx.x / 8) + k_offset;
+        int effective_x = (threadIdx.x % 8) + (x_offset / 4);
+        des4[threadIdx.x] = src4[effective_k * (n / 4) + effective_x];
+    } else {
         if (threadIdx.x < 32) {
             des[threadIdx.x] = src[threadIdx.x + x_offset];
         }
@@ -34,17 +38,31 @@ __device__ __forceinline__ void fill_input(const float *__restrict__ src, float 
 
 template <bool Batch>
 __device__ __forceinline__ void accumulate(const float *__restrict__ weights, float *__restrict__ input, float *__restrict__ sums) {
-    int row = (threadIdx.x / 8);
-    int col = (threadIdx.x % 8) * 4;
-    float sum = 0.0f;
-    for (int i = 0; i < 4; i++) {
-        sum += weights[row * 32 + col + i] * input[col + i];
-    }
-    for (int offset = 4; offset > 0; offset /= 2) {
-        sum += __shfl_down_sync(SHUFFLE_MASK, sum, offset);
-    }
-    if (threadIdx.x % 8 == 0) {
-        sums[row] += sum;
+    if constexpr (Batch) {
+        // 1 thread per 4 rows
+        int start_row = (threadIdx.x % 8) * 4;
+        int depth = (threadIdx.x / 8);
+        for (int row = start_row; row < start_row + 4; row++) {
+            float sum = 0.0f;
+            for (int col = 0; col < 32; col++) {
+                sum += weights[row * 32 + col] * input[depth * 32 + col];
+            }
+            sums[depth * 32 + row] = sum;
+        }
+    } else {
+        // 8 threads per 1 row
+        int row = (threadIdx.x / 8);
+        int col = (threadIdx.x % 8) * 4;
+        float sum = 0.0f;
+        for (int i = 0; i < 4; i++) {
+            sum += weights[row * 32 + col + i] * input[col + i];
+        }
+        for (int offset = 4; offset > 0; offset /= 2) {
+            sum += __shfl_down_sync(SHUFFLE_MASK, sum, offset);
+        }
+        if (threadIdx.x % 8 == 0) {
+            sums[row] += sum;
+        }
     }
 }
 
@@ -71,19 +89,17 @@ __global__ void linear_forward_mtm(const float *__restrict__ input, const float 
 
     // Each block takes care of a row of tiles at a time then moves to another row
     for (int tile_y = blockIdx.x; tile_y < tile_m; tile_y += gridDim.x) {
+        int tile_y_offset = tile_y * TILE_HEIGHT;
         // clear accumulators
         if (threadIdx.x < TILE_HEIGHT) {
             sums[threadIdx.x] = 0.0f;
         }
 
         for (int tile_x = 0; tile_x < tile_n; tile_x++) {
-
-            int tile_y_offset = tile_y * TILE_HEIGHT;
             int tile_x_offset = tile_x * TILE_WIDTH;
-            int tile_k_offset;
 
             fill_weights(weights, weight_cache, tile_x_offset, tile_y_offset, n);
-            fill_input<false>(input, input_cache, tile_x_offset, tile_k_offset, n);
+            fill_input<false>(input, input_cache, tile_x_offset, 0, n);
             __syncthreads();
             accumulate<false>(weight_cache, input_cache, sums);
             __syncthreads();
@@ -96,6 +112,66 @@ __global__ void linear_forward_mtm(const float *__restrict__ input, const float 
             output[output_index] = Activation::apply(sums[threadIdx.x] + biases[output_index]);
         }
     }   
+}
+
+// shape.n must be divisible by 32, shape.m must be divisible by 32.
+// blockDim.x must equal 256
+// Failing to meet any of the required precondition will result in undefined behavior
+template <typename Activation = Identity>
+__global__ void linear_forward_mtm_batch(const float *__restrict__ input, const float *__restrict__ weights, const float *__restrict__ biases, float *__restrict__ output, MatrixMultShape shape) {
+    
+    // setup
+    const int THREADS_PER_BLOCK = 256;
+    const int TILE_WIDTH = 32;
+    const int TILE_HEIGHT = 32;
+    const int TILE_DEPTH = 32;
+
+    const int m = shape.output;
+    const int n = shape.input;
+    const int k = shape.batch_size;
+
+    const int tile_m = m / TILE_HEIGHT;
+    const int tile_n = n / TILE_WIDTH;
+    const int tile_k = k / TILE_DEPTH;
+    
+    __shared__ float weight_cache[TILE_WIDTH * TILE_HEIGHT];
+    __shared__ float input_cache[TILE_WIDTH * TILE_DEPTH];
+    __shared__ float sums[TILE_HEIGHT * TILE_DEPTH];
+
+    for (int tile_z = blockIdx.y; tile_z < tile_k; tile_z += gridDim.y) {
+        int tile_k_offset = tile_z * TILE_DEPTH;
+        // Each block takes care of a row of tiles at a time then moves to another row
+        for (int tile_y = blockIdx.x; tile_y < tile_m; tile_y += gridDim.x) {
+            int tile_y_offset = tile_y * TILE_HEIGHT;
+            // clear accumulators
+            if (threadIdx.x < TILE_HEIGHT) {
+                sums[threadIdx.x] = 0.0f;
+            }
+
+            for (int tile_x = 0; tile_x < tile_n; tile_x++) {
+
+                int tile_x_offset = tile_x * TILE_WIDTH;
+
+                fill_weights(weights, weight_cache, tile_x_offset, tile_y_offset, n);
+                fill_input<true>(input, input_cache, tile_x_offset, tile_k_offset, n);
+                __syncthreads();
+                accumulate<true>(weight_cache, input_cache, sums);
+                __syncthreads();
+            }
+
+            int output_y = tile_y;
+            for (int i = 0; i < 4; i++) {
+                output[threadIdx.x * 4 + i] 
+            }
+            
+            // The entire row of tiles now has it's sum in sums.
+            // Move to output and add bias
+            if (threadIdx.x < TILE_HEIGHT) {
+                int output_index = tile_y * TILE_HEIGHT + threadIdx.x;
+                output[output_index] = Activation::apply(sums[threadIdx.x] + biases[output_index]);
+            }
+        }
+    }
 }
 
 #endif
